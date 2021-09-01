4c54b8f1f37a650147a0e5edce23bd9cb355ecf2
Geo3777 <87963537+Geo3777@users.noreply.github.com>
2021-09-01 12:28:18 +0300

    restSharp

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 
 namespace AspNetSandbox.Controllers
 {
@@ -26,6 +27,28 @@ namespace AspNetSandbox.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            var client = new RestClient("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b509889970fc2b771cf13e4dfde7d0ee");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            return ConvertResponseToWeatherForecast(response.Content);
+
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+
+            //http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b509889970fc2b771cf13e4dfde7d0ee
+            //}
+        }
+
+        private IEnumerable<WeatherForecast> ConvertResponseToWeatherForecast(string Content)
+        {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -34,8 +57,6 @@ namespace AspNetSandbox.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
-
-            //http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b509889970fc2b771cf13e4dfde7d0ee
         }
     }
 }
