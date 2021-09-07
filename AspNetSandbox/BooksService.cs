4bc3dec6e96158cf3b477cbac495b378e0e43d7a
Geo3777 <87963537+Geo3777@users.noreply.github.com>
2021-09-07 13:52:38 +0300

    BooksService class

---
+++ AspNetSandbox/BooksService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetSandbox
+{
+    public class BooksService
+    {
+        private List<Book> books;
+        public BooksService()
+        {
+            books = new List<Book>();
+            books.Add(new Book
+            {
+                Id = 1,
+                title = "War Hero",
+                Language = "English",
+                author = "Sven"
+            });
+
+            books.Add(new Book
+            {
+                Id = 2,
+                title = "War Hero",
+                Language = "English",
+                author = "Sven"
+            });
+
+        }
+
+        public IEnumerable<Book> Get()
+        {
+            return books;
+        }
+
+        public Book Get(int id)
+        {
+            return books.Single(book => book.Id == id);
+        }
+
+        public void Post(Book value)
+        {
+            int id = books.Count + 1;
+            value.Id = id;
+            books.Add(value);
+        }
+
+        public void Put(int id, string value)
+        {
+
+        }
+
+        public void Delete(int id)
+        {
+            books.Remove(Get(id));
+        }
+    }
+}
