e35f33bbeb6ed275fb5bf0c5ac311d2dcfa43980
Geo3777 <87963537+Geo3777@users.noreply.github.com>
2021-09-07 15:37:51 +0300

    Updated Sturtup #2

---
+++ AspNetSandbox.text/BooksServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetSandbox.Tests
+{
+    class BooksServiceTest
+    {
+        public static void ShouldHaveLondonCoordinates()
+        {
+            // Assume
+            var booksService = new BooksService();
+
+
+            // Act
+            booksService.Post(new Book
+            {
+                title = "Psyho ABC",
+                author = "John",
+                Language = "Romanian"
+            });
+
+            booksService.Delete(2);
+            booksService.Post(new Book
+            {
+                title = "The art of not giving a f..",
+                author = "Tom example",
+                Language = "English"
+            });
+
+            //Assert
+            Assert.Equal("Psyho ABC", booksService.Get(3).title);
+        }
+    }
+}
