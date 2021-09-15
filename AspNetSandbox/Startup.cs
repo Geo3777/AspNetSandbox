76aee4cf70f07335bfda435c6ab29b16c4f2ad85
Geo3777 <87963537+Geo3777@users.noreply.github.com>
2021-09-15 11:20:52 +0300

    moddifed

--- AspNetSandbox/Startup.cs
+++ AspNetSandbox/Startup.cs
@@ -94,9 +94,9 @@ namespace AspNetSandbox
 
             app.UseEndpoints(endpoints =>
             {
-                // endpoints.MapControllerRoute(
-                // name: "default",
-                //pattern: "{controller=Home}/{action=Index}/{id?}");
+				endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
                 endpoints.MapControllers();
                 endpoints.MapHub<MessageHub>("/messagehub");
