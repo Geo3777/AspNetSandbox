94f2e4aa9d8d33c370de565158798dbb80945bc9
Geo3777 <87963537+Geo3777@users.noreply.github.com>
2021-09-21 15:59:24 +0300

    Better data to be added if no books are in dtabase

--- AspNetSandbox/Data/DataTools.cs
+++ AspNetSandbox/Data/DataTools.cs
@@ -24,8 +24,16 @@ namespace AspNetSandbox.Data
                 {
                     Console.WriteLine("No books");
                     var book1 = new Book();
+                    book1.Id = 1;
+                    book1.Title = "The Old Book";
+                    book1.Author = "George";
+                    book1.Language = "English";
                     applicationDbContext.Add(book1);
                     var book2 = new Book();
+                    book2.Id = 2;
+                    book2.Title = "The New Book";
+                    book2.Author = "Otgon";
+                    book2.Language = "English";
                     applicationDbContext.Add(book2);
                     applicationDbContext.SaveChanges();
                 }
